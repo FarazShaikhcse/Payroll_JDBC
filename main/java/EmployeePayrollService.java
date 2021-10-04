ad177dc86733e345b95eed998d0dc4c57287b297
Faraz Shaikh <farazshabbir@113AMBBLR-Faraz-Shabbir-Shaikh.local>
2021-10-04 20:03:29 +0530

    read data from database

--- main/java/EmployeePayrollService.java
+++ main/java/EmployeePayrollService.java
@@ -9,30 +9,56 @@ import com.mysql.cj.jdbc.Driver;
 
 public class EmployeePayrollService {
 
-	public void connectDatabase() {
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			System.out.println("Driver loaded");
-			Connection con;
-			try {
-				con = DriverManager.getConnection("jdbc:mysql://localhost:3306/payroll", "root", "faraz962495");
-				System.out.println("connected to database" + con);
-				listDrivers();
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
 
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
+
+	public Connection connectDatabase() throws ClassNotFoundException, SQLException {
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		System.out.println("Driver loaded");
+		Connection connection = null;
+		connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/payroll_service", "root", "faraz962495");
+		System.out.println("connected to database" + connection);
+		return connection;
 
 	}
 
+	/**
+	 * method to establish connection with employee payroll database
+	 * 
+	 * @throws SQLException
+	 * @throws ClassNotFoundException
+	 */
+	public void checkDatabaseConnection() throws ClassNotFoundException, SQLException {
+		Connection connection = connectDatabase();
+		listDrivers();
+
+	}
+
+	/**
+	 * method to list the drivers
+	 */
 	private void listDrivers() {
 		Enumeration<java.sql.Driver> driverList = DriverManager.getDrivers();
 		while (driverList.hasMoreElements()) {
 			Driver driverClass = (Driver) driverList.nextElement();
 			System.out.println(" " + driverClass.getClass().getName());
 		}
+	}
+
+	/**
+	 * method to select data from the database
+	 * 
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 */
+	public void queryData() throws ClassNotFoundException, SQLException {
+		Connection connection = connectDatabase();
+		Statement statement = connection.createStatement();
+		ResultSet resultSet = statement.executeQuery("select * from employee_payroll");
+		while (resultSet.next()) {
+			System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2) + " " + resultSet.getInt(3) + " "
+					+ resultSet.getDate(4));
+		}
+
 	}
 }
